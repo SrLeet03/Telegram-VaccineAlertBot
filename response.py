b940045f0c71046df5896eba3c0186f677bcd93d
Sarvesh raut <59080732+SrLeet03@users.noreply.github.com>
2021-05-25 10:20:15 +0530

    Added Responses by the Bot

---
+++ response.py
@@ -0,0 +1,66 @@
+import requests, json
+
+browser_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+
+
+def District_List(text):
+    var = int(text)
+    url = f'https://cdn-api.co-vin.in/api/v2/admin/location/districts/{var}'
+    response = requests.get(url, headers=browser_header)
+    json_data = response.json()
+    final_text = ''
+    if len(json_data['districts'])==0:
+           # no = 
+           # no = no + pin + "on" + date
+        return 'No districts for the given state'
+    else:
+        for slots in json_data['districts']:
+            final_text = final_text + "\nName: "+str(slots['district_name']) +' , '+ "District_Id: "+str(slots['district_id']) +'\n'
+            #final_text = final_text + '----------------------------------------'
+
+    return final_text
+
+def myfuc():
+    return "Hey There,\nI can help you to book your Cvoid19 Vaccination slot!\nList of my sevices:\n1.Know The Available slots of covid doses in your distric , For steps to follow Enter help"
+
+
+def sample(input_text):
+    f = 1
+    for i in input_text:
+        if i==':' :
+            f = 0
+
+    if(f):
+        s = "Invalid Input"
+        s+="If you want to know the vaccination slots for your district then the input format is \n District_code:DD-MM-YR , Eg.432:03-05-2001\n If You want to Know your District Code Just Hit \n help"
+        return s
+    user_message = str(input_text).lower()
+    #print(user_message)
+    new_data = user_message.split(':')
+    pin = new_data[0]
+    date = new_data[1]
+    try:
+        pin = int(pin)
+    except Exception as e:
+        pass
+    if(int(pin)<10000 and int(pin)>0):
+
+        url = f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={pin}&date={date}'
+       
+        #print(url)
+        response = requests.get(url, headers=browser_header)
+        #print(response)
+        json_data = response.json()
+        final_text = ''
+        if len(json_data['sessions'])==0:
+           # no = 
+           # no = no + pin + "on" + date
+            return 'Slots Not Available for the selected District'
+        else:
+            for slots in json_data['sessions']:
+                final_text = final_text + "\nName: "+str(slots['name']) +'\n'+ "Available Capacity: "+str(slots['available_capacity']) +'\n' + "Min Age Limit: "+str(slots['min_age_limit']) +'\n' + "Vaccine: "+str(slots['vaccine'])+ '\n'
+                final_text = final_text + '----------------------------------------'
+
+        return final_text
+    else:
+        return "Invalid input"
