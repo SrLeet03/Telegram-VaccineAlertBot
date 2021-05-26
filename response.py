c9acc8c1f006afeb447718d952b20926056b78de
Sarvesh raut <59080732+SrLeet03@users.noreply.github.com>
2021-05-26 11:58:02 +0530

    Update response.py

--- response.py
+++ response.py
@@ -1,10 +1,10 @@
 import requests, json
-
+import constants as Cons
 browser_header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
 
 def ListOfState():
     url = f'https://cdn-api.co-vin.in/api/v2/admin/location/states'
-    response = requests.get(url, headers=browser_header)
+    response = requests.get(url, headers=Cons.browser_header)
     json_data = response.json()
     final_text = ''
     
@@ -16,7 +16,7 @@ def ListOfState():
 def District_List(text):
     var = int(text)
     url = f'https://cdn-api.co-vin.in/api/v2/admin/location/districts/{var}'
-    response = requests.get(url, headers=browser_header)
+    response = requests.get(url, headers=Cons.browser_header)
     json_data = response.json()
     final_text = ''
     if len(json_data['districts'])==0:
@@ -43,7 +43,8 @@ def sample(input_text):
     if(f):
         s = "Invalid Input"
         s+="If you want to know the vaccination slots for your district then the input format is \n District_code:DD-MM-YR , Eg.432:03-05-2001\n If You want to Know your District Code Just Hit \n help"
-        return s
+        brrr = [s]
+        return brrr
     user_message = str(input_text).lower()
     #print(user_message)
     new_data = user_message.split(':')
@@ -58,7 +59,7 @@ def sample(input_text):
         url = f'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={pin}&date={date}'
        
         #print(url)
-        response = requests.get(url, headers=browser_header)
+        response = requests.get(url, headers=Cons.browser_header)
         #print(response)
         json_data = response.json()
         final_text = ''
@@ -69,13 +70,19 @@ def sample(input_text):
 
             return brr
         else:
+            #print('chejdfasdvasv')
             for slots in json_data['sessions']:
                 final_text = final_text + "\nName: "+str(slots['name']) +'\n'+ "Available Capacity: "+str(slots['available_capacity']) +'\n' + "Min Age Limit: "+str(slots['min_age_limit']) +'\n' + "Vaccine: "+str(slots['vaccine'])+ '\n'
                 if(slots['available_capacity']>0):
                     final_text = final_text + '----------------------------------------'
                     arr.append(final_text)
+                    #print('chejdfasdvasv')
+                    #print(final_text)
                 final_text = ''
-        #print(final_text)
+        if(len(arr)==0):
+            return brr
         return arr
     else:
-        return "Invalid input"
+        sick = "Invalid_input"
+        brr = [sick]
+        return brr
